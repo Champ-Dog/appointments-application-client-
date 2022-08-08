19fc5b7bb993e8638b7e236ebe5299b6147d9e0d
Champ-Dog <champdog.code@gmail.com>
2022-08-08 14:13:59 +1000

    Configured login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,29 @@
-import { Form, Input, Button } from "antd";
+import { Button, Form, Input } from "antd";
+import axios from "axios";
 import React from "react";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 function Login() {
-  const onFinish = (values) => {
-    console.log("Received values: ", values);
+  const navigate = useNavigate();
+  const onFinish = async (values) => {
+    try {
+      const response = await axios.post(`/api/user/login`, values);
+      if (response.data.success) {
+        toast.success(response.data.message);
+        toast("Redirecting to Home");
+
+        // Store token in localStorage - in backend, 'data' is the token, hence 'response.data.data', not 'response.data.token
+        localStorage.setItem("token", response.data.data);
+
+        navigate("/");
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      toast.error("Something went wrong");
+    }
   };
 
   return (
